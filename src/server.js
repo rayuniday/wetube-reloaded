5afb121cb60faf31d99ec5d9f5470299c55eb8d9
rayuniday <97986074+rayuniday@users.noreply.github.com>
2022-01-22 06:57:14 +0900

    #3.5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,14 +4,19 @@ const PORT = 4000;
 
 const app = express()
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
+
 const handleHome = (req, res) => {
-    return res.end()
+    return res.send("I love middlewares")
 }
 const handleLogin = (req, res) => {
     return res.send({ message: "Login here." })
 }
 
-app.get("/", handleHome)
+app.get("/", gossipMiddleware, handleHome)
 app.get("/login", handleLogin)
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
