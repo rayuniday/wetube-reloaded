4b01ca61b33d17fc7186c8041c2763a4a66e32e4
rayuniday <rayuniday@naver.com>
2022-02-16 09:40:20 +0900

    #3.10 Middleware Recap

--- src/server.js
+++ src/server.js
@@ -5,17 +5,24 @@ const PORT = 4000;
 const app = express();
 
 const routerLogger = (req, res, next) => {
-  return res.send("www");
-};
-const methodLogger = (req, res, next) => {
+  console.log("PATH", req.path);
   next();
 };
-
+const methodLogger = (req, res, next) => {
+  console.log("METHOD", req.method);
+  next();
+};
 const home = (req, res) => {
+  console.log("I will respond.");
   return res.send("hello");
 };
+const login = (req, res) => {
+  return res.send("login");
+};
 
-app.get("/", methodLogger, routerLogger, home);
+app.use(methodLogger, routerLogger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
