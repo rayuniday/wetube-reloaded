5449ffef75523cdd80d504e839bef08764cedcc9
rayuniday <rayuniday@naver.com>
2022-02-16 09:33:09 +0900

    #3.9 Controllers Recap

--- src/server.js
+++ src/server.js
@@ -4,9 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("home");
+const routerLogger = (req, res, next) => {
+  return res.send("www");
+};
+const methodLogger = (req, res, next) => {
+  next();
+};
 
-app.get("/", handleHome);
+const home = (req, res) => {
+  return res.send("hello");
+};
+
+app.get("/", methodLogger, routerLogger, home);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
