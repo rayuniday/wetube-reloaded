b63156e8e09fad2f747d41655fa1888f02502464
rayuniday <rayuniday@naver.com>
2022-02-22 07:04:23 +0900

    #6.8 Connecting to Mongo

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 
@@ -19,6 +20,6 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
