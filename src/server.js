9f19bc54b4576dbc359469e777833e442af42592
rayuniday <rayuniday@naver.com>
2022-02-22 08:13:19 +0900

    #6.12 Our First Query

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
-import "./db";
 import express from "express";
 import morgan from "morgan";
 
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -19,7 +16,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
