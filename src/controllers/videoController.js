082391ab5c408f304221e9e85801c6273e9186fc
rayuniday <rayuniday@naver.com>
2022-02-22 08:37:53 +0900

    #6.13 Async Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,21 @@
 import Video from "../models/Video";
-
-export const home = (req, res) => {
-  console.log("Start");
-  Video.find({}, (error, videos) => {
-    console.log("Finished");
-    return res.render("home", { pageTitle: "Home", videos });
-  });
+/* callback
+Video.find({}, (error, videos) => {
+  if(error) {
+    return res.render("server-error");
+  }
+  return res.render("home", { pageTitle: "Home", videos })
+})
+*/
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
+  // try {
+  //   const videos = await Video.find({});
+  //   return res.render("home", { pageTitle: "Home", videos });
+  // } catch (error) {
+  //   return res.render("server-error", { error });
+  // }
 };
 export const watch = (req, res) => {
   const { id } = req.params;
