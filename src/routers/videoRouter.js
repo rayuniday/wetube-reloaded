35ee8c99a5f7ca5d43f793e7b77686536077f677
rayuniday <rayuniday@naver.com>
2022-02-22 05:55:33 +0900

    #6.6 More Practice part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,9 @@ import {
   watch,
   getEdit,
   postEdit,
-  upload,
   deleteVideo,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,6 +13,6 @@ const videoRouter = express.Router();
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
